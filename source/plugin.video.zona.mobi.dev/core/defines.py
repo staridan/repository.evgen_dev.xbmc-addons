6bb7f9edd662180c1560cd51dd285be1af6f4040
Ivan Kotenev <st.aridan@gmail.com>
2018-01-02 11:56:33 +0300

    removed unrelated to filmix.cc addons

--- source/plugin.video.zona.mobi.dev/core/defines.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-SITE_DOMAIN = 'zona.mobi'
-SITE_URL = 'http://'+SITE_DOMAIN
-PLUGIN_ID = 'plugin.video.zona.mobi.dev'
-CACHE_DATABASE = 'zonamobi.cache.db'
-COOKIE_FILE = 'zonamobi_cookie.txt'
-
-QUALITYS = [None, '360', '480', '720', '1080']
